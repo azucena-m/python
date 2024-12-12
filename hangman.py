b4a55bf33fe7917da12a5124dd50786e23cb3cf0
Azucena Marroquin <atrevizo4@cnm.edu>
2024-12-12 10:37:24 -0700

    practice

---
+++ hangman.py
@@ -0,0 +1,27 @@
+from words import words 
+import random
+import string
+
+def get_valid_word(words):
+    word = random.choice(words)
+    while '-' in word or ' ' in word:
+        word = random.choice(words)
+    
+    return word
+
+def hangmain():
+    word = get_valid_word(words)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase)
+    used_letters = set()
+
+    user_letter = input('Guess a letter: ').upper()
+
+    if user_letter in alphabet - used_letters:
+        used_letters.add(user_letter)
+        if user_letter in word_letters:
+            word_letters.remove(user_letter)
+    elif user_letter in used_letters:
+        print('You have already used that character. Please try again. ')
+    else:
+        print('Invalid character. Please try again. ')
